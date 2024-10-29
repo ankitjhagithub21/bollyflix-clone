ab137c70f8139e64690054fa3ad1d394fa1f5438
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-29 21:06:10 +0530

    minor changes

--- client/src/components/custom/MovieCard.jsx
+++ client/src/components/custom/MovieCard.jsx
@@ -4,8 +4,8 @@ const MovieCard = ({ movie }) => {
   return (
     <div className='movie-card overflow-hidden cursor-pointer'>
       <img src={movie.screenshots[1]} alt="thumbnail" className='h-64 object-cover' />
-      <div className='bg-[#27272A] p-3  pb-10'>
-        <p className='text-sm text-white text-center'>Download {movie.fullName} {movie.year} {movie.language} Movie {movie.quality} </p>
+      <div className='bg-[#27272A] p-3  h-32 overflow-hidden'>
+        <p className='text-sm text-white text-center'>Download {movie.fullName} {movie.year} {movie.quality} </p>
       </div>
     </div>
   )
