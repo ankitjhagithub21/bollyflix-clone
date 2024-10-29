9dc7d06f94d9a7e46c07142d8f7d2fbef6884db5
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-29 21:36:25 +0530

    minor changes

--- client/src/components/custom/Movies.jsx
+++ client/src/components/custom/Movies.jsx
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard'
 const Movies = () => {
   const {movies} = useFetchMovies()
   return (
-    <div className='max-w-6xl mx-auto grid gap-8 my-12 lg:grid-cols-5 md:grid-cols-3 grid-cols-3'>
+    <div className='max-w-6xl mx-auto min-h-screen items-start grid gap-8 my-12 lg:grid-cols-5 md:grid-cols-3 grid-cols-3'>
       {
         movies.map((movie)=>{
           return <MovieCard key={movie._id} movie={movie}/>
