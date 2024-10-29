d3f356cc82f1b8b7b153b1f660bb158553cb0ded
ankitjhagithub21 <ajha45075@gmail.com>
2024-10-29 23:08:40 +0530

    minor changes

--- client/src/pages/MovieResults.jsx
+++ client/src/pages/MovieResults.jsx
@@ -8,29 +8,35 @@ const MovieResults = () => {
     const { title } = useParams()
     const [movies, setMovies] = useState([])
 
-   useEffect(()=>{
-    const fetchMoviesByTitle = async() => {
-        try {
-            const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/movies/title/${title}`)
-            const data = await res.json()
-            if (data.success) {
-                setMovies(data.movies)
+    useEffect(() => {
+        const fetchMoviesByTitle = async () => {
+            try {
+                const res = await fetch(`${import.meta.env.VITE_SERVER_URL}/api/movies/title/${title}`)
+                const data = await res.json()
+                if (data.success) {
+                    setMovies(data.movies)
+                }else{
+                    setMovies([])
+                }
+            } catch (error) {
+                console.log(error)
             }
-        } catch (error) {
-            console.log(error)
         }
-    }
-    fetchMoviesByTitle()
-   },[title])
-return (
-    <div className='max-w-6xl mx-auto'>
-        <Navbar />
-        <div className='min-h-screen'>
-            <Movies movies={movies} />
+        fetchMoviesByTitle()
+    }, [title])
+
+    return (
+        <div className='max-w-6xl mx-auto'>
+            <Navbar />
+            <div className='min-h-screen'>
+                {
+                    movies.length === 0 ? <h2 className='text-center text-white my-10 text-3xl font-semibold'>No Movie Found</h2> : <Movies movies={movies} />
+
+                }
+            </div>
+            <Footer />
         </div>
-        <Footer />
-    </div>
-)
+    )
 }
 
 export default MovieResults
